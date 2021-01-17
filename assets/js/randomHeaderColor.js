acbfe81aa8d24e77b1da35f565b20d2962e06bfc
Leandro Lapeña <lapenaleandro@gmail.com>
2021-01-17 16:42:45 +0800

    removed redundant meta

--- assets/js/randomHeaderColor.js
+++ assets/js/randomHeaderColor.js
@@ -2,9 +2,39 @@ function getNewRandomColor() {
   var pheadcolors = ["#41eded", "#ed9898", "#60a2d3", "#c8c8c8", "#fff491"];
   var sheadcolors = ["#ed4141", "#98eded", "#d39160", "#00ffa1", "#919cff"];
 
-  var dark_pri = ["#2A3742", "#412A42", "#005D63", "#42352A", "#2B422A", "#438066", "#A6FFFD", "#FDA6FF", "#FFFDA6"];
-  var dark_sec = ["#2A3742", "#412A42", "#002C63", "#42352A", "#2B422A", "#437C80", "#A6FFFD", "#FDA6FF", "#FFFDA6"];
-  var dark_ter = ["#2A3742", "#412A42", "#006337", "#42352A", "#2B422A", "#438047", "#A6FFFD", "#FDA6FF", "#FFFDA6"]; 
+  var dark_pri = [
+    "#2A3742",
+    "#412A42",
+    "#005D63",
+    "#42352A",
+    "#2B422A",
+    "#438066",
+    "#A6FFFD",
+    "#FDA6FF",
+    "#FFFDA6",
+  ];
+  var dark_sec = [
+    "#2A3742",
+    "#412A42",
+    "#002C63",
+    "#42352A",
+    "#2B422A",
+    "#437C80",
+    "#A6FFFD",
+    "#FDA6FF",
+    "#FFFDA6",
+  ];
+  var dark_ter = [
+    "#2A3742",
+    "#412A42",
+    "#006337",
+    "#42352A",
+    "#2B422A",
+    "#438047",
+    "#A6FFFD",
+    "#FDA6FF",
+    "#FFFDA6",
+  ];
 
   var degs = ["0deg", "45deg", "60deg", "90deg", "120deg"];
   var deg = degs[Math.floor(Math.random() * degs.length)];
@@ -23,32 +53,22 @@ function getNewRandomColor() {
       "background-image",
       "linear-gradient(" + deg + "," + secondary + "," + primary + ")"
     );
-  $("meta").each(function () {
-    if ($(this).attr("name") == "theme-color") {
-      $(this).attr("content", primary);
-    }
-  });
-  $(document).ready(function () {
-    $(".my-svg-icon").hover(
-      function () {
-        $(this).css("fill", secondary);
-      },
-      function () {
-        $(this).css("fill", primary);
-      }
-    );
-
-    $(".my-svg-icon").css("fill", primary);
-  });
   } else if (head_type == 1 && deg != "0deg") {
     $(".page-header").css(
       "background-image",
-      "linear-gradient(" + deg + ", " + dark_3 + ", " + dark_2 + ", " + dark_1 + ")"
+      "linear-gradient(" +
+        deg +
+        ", " +
+        dark_3 +
+        ", " +
+        dark_2 +
+        ", " +
+        dark_1 +
+        ")"
     );
-    
-    
-  }$("meta").each(function () {
-      if ($(this).attr("name") == "theme-color") {
+  }
+  $("meta").each(function () {
+    if ($(this).attr("name") == "theme-color") {
       $(this).attr("content", dark_3);
     }
   });
@@ -60,11 +80,11 @@ function getNewRandomColor() {
       function () {
         $(this).css("fill", dark_3);
       }
-      );
-      
-      $(".my-svg-icon").css("fill", dark_3);
-    });
-    // tags = document.getElementsByTagName('h3');
+    );
+
+    $(".my-svg-icon").css("fill", dark_3);
+  });
+  // tags = document.getElementsByTagName('h3');
   // var i = 0;
   // while (tags[i]) {
   //   tags[i].style.color = rand;
