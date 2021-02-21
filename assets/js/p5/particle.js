02ecb254a40025b446d1c4e835b4d8b4f9232d87
Leandro Lapeña <lapenaleandro@gmail.com>
2021-02-21 23:53:28 +0800

    field colors

--- assets/js/p5/particle.js
+++ assets/js/p5/particle.js
@@ -1,39 +1,67 @@
+var meta_primary_color;
 function Particle() {
-  this.pos = createVector(random(width), random(height));
-//   this.vel = p5.Vector.random2D();
-  this.vel = createVector(0,0);
-  this.acc = createVector(0, 0);
-  this.maxspeed = 4;
+    $("meta").each(function () {
+        if ($(this).attr("name") == "theme-color") {
+            meta_primary_color = $(this).attr("content"); // 50%	80	128 60% 99
+        }
+    });
+    this.pos = createVector(random(width), random(height));
+    //   this.vel = p5.Vector.random2D();
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.maxspeed = 4;
+    this.prevPos = this.pos.copy();
+    
+    this.update = function () {
+        this.vel.add(this.acc);
+        this.vel.limit(this.maxspeed);
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+    };
+    
+    this.follow = function (vectors) {
+        var x = floor(this.pos.x / scl);
+        var y = floor(this.pos.y / scl);
+        var index = x + y * cols;
+        var force = vectors[index];
+        this.applyForce(force);
+    };
+    
+    this.applyForce = function (force) {
+        this.acc.add(force);
+    };
+    
+    this.show = function () {
+        setMetaColor = color(meta_primary_color);
+        setMetaColor.setAlpha(20);
+        stroke(setMetaColor);
+        strokeWeight(1);
+        line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+        // point(this.pos.x, this.pos.y);
+        this.updatePrev();
+    };
 
-  this.update = function () {
-    this.vel.add(this.acc);
-    this.vel.limit(this.maxspeed);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
+this.updatePrev = function () {
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
+};
+
+this.edges = function () {
+    if (this.pos.x > width) {
+        this.pos.x = 0;
+        this.updatePrev();
+    }
+    if (this.pos.x < 0) {
+        this.pos.x = width;
+        this.updatePrev();
+    }
+    if (this.pos.y > height) {
+        this.pos.y = 0;
+        this.updatePrev();
+    }
+    if (this.pos.y < 0) {
+        this.pos.y = height;
+        this.updatePrev();
+    }
   };
-
-  this.follow = function(vectors){
-      var x = floor(this.pos.x / scl);
-      var y = floor(this.pos.y / scl);
-      var index = x + y * cols;
-      var force = vectors[index];
-      this.applyForce(force);
-  }
-
-  this.applyForce = function (force) {
-    this.acc.add(force);
-  };
-
-  this.show = function () {
-    stroke(0);
-    strokeWeight(4);
-    point(this.pos.x, this.pos.y);
-  };
-
-  this.edges = function(){
-      if (this.pos.x > width) this.pos.x = 0;
-      if (this.pos.x < 0) this.pos.x = width;
-      if (this.pos.y > height) this.pos.y = 0;
-      if (this.pos.y < 0) this.pos.y = height;
-  }
 }
--- assets/js/p5/perlinField.js
+++ assets/js/p5/perlinField.js
@@ -7,40 +7,40 @@ var zoff = 0;
 var particles = [];
 var flowfield;
 
-setup = () => {
-  createCanvas(200, 200);
-  // cnv.parent("perlin-canvas");
+function setup() {
+  let cnv = createCanvas(300, 300);
+  cnv.parent("perlin-canvas");
   cols = floor(width / scl);
   rows = floor(height / scl);
   fr = createP("");
   flowfield = new Array(cols * rows);
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 500; i++) {
     particles[i] = new Particle();
   }
+  background(255);
 };
 
 draw = () => {
-  background(255);
   var yoff = 0;
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
       var index = x + y * cols;
-      var angle = noise(xoff, yoff, zoff) * two_pi;
+      var angle = noise(xoff, yoff, zoff) * two_pi * 3;
       var v = p5.Vector.fromAngle(angle);
-      v.setMag(0.1);
+      v.setMag(0.5);
       flowfield[index] = v;
       xoff += inc;
       // fill(sketch.random(255));
       // fill(r);
       // rect(x * scl, y * scl, scl, scl);
       stroke(0, 50);
-      push();
-      translate(x * scl, y * scl);
-      rotate(v.heading());
-      strokeWeight(1);
-      line(0, 0, scl, 0);
-      pop();
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // strokeWeight(1);
+      // line(0, 0, scl, 0);
+      // pop();
     }
     yoff += inc;
     zoff += 0.0005;
@@ -48,8 +48,8 @@ draw = () => {
   for (var i = 0; i < particles.length; i++) {
     particles[i].follow(flowfield);
     particles[i].update();
-    particles[i].show();
     particles[i].edges();
+    particles[i].show();
   }
   fr.html(floor(frameRate()));
 };
